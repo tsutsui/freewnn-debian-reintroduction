dc4ae760fa346a6e2cadf81fbb0bb70896dd5ab4
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2026-07-09 01:04:05 +0900

    Wnn/romkan/rk_main.c: Appease -Wchar-subscripts

--- Wnn/romkan/rk_main.c
+++ Wnn/romkan/rk_main.c
@@ -88,7 +88,7 @@ char eofflg;                    /* romkan_next()からLTREOFが来たときに、romkan_h
                                    処理中はeofflgは2。 */
 
 letter evalbuf[2][2][OUTSIZ];
-char ebf_sw = 0;
+int ebf_sw = 0;
 letter saishu_out[OUTSIZ];
 letter delchr, delchr2 = EOLTTR, nisedl;        /* DELのキャラクタは二個まで持てる */
 
