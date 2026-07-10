49963ccc1986b5c8e17c4fa65d38cd0faf1523ab
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2026-07-10 23:41:58 +0900

    Wnn/jserver/b_index.c: Appease -Wpointer-compare

--- Wnn/jserver/b_index.c
+++ Wnn/jserver/b_index.c
@@ -314,7 +314,7 @@ bnode_alloc (struct JT *jt)
       return (i);
     }
   if (b_cnt++ >= jt->bufsize_bnode)     /* Use new  b_nodes */
-    if (rd_realloc_bind (jt) == NULL)	/* realloc jt->bind */
+    if (rd_realloc_bind (jt) == 0)	/* realloc jt->bind */
       return (-1);
   return (jt->bufsize_bnode = b_cnt);   /* Not re-alloc */
 }
