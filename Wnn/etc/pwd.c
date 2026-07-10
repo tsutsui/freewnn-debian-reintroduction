d582d0ea7dfd937d56f911ee5e1695d45dcc9288
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2026-07-10 22:48:37 +0900

    Wnn/etc/pwd.c: include <time.h> unconditionally for time(3)

--- Wnn/etc/pwd.c
+++ Wnn/etc/pwd.c
@@ -36,6 +36,8 @@
 #  include <config.h>
 #endif
 
+#include <time.h>
+
 #ifndef JS
 #include <stdio.h>
 #if STDC_HEADERS
@@ -46,7 +48,6 @@
 #if HAVE_UNISTD_H
 #  include <unistd.h>
 #endif
-#include <time.h>
 
 #include "commonhd.h"
 #include "jslib.h"
